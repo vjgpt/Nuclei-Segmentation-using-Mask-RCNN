3bf78170b4f4f9849275075ef98f31fdb3120468
Vijay <vjgupta57@gmail.com>
2018-12-03 15:19:35 +0530

    Prepare files

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ import skimage.io
 from imgaug import augmenters as iaa
 
 # Root directory of the project
-ROOT_DIR = os.path.abspath("")
+ROOT_DIR = os.path.abspath("../../")
 
 # Import Mask RCNN
 sys.path.append(ROOT_DIR)  # To find local version of the library
